2b7ba00d787ffa83baf5593d8285ac4d375f9b30
schwamic <schwarz.michael@posteo.net>
2024-02-09 17:21:42 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  generateBuildId: async () => {
-    // This could be anything, using the latest git hash
-    return process.env.GIT_HASH;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
